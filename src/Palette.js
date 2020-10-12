6cbb36316d35635f3e3ecb5af110ba97225cf59e
yarinTag <taguori5@gmai.com>
2020-10-12 22:28:30 +0300

    created react app,added palette

---
+++ src/Palette.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Palette extends Component{
+    render(){
+        return(
+            <div className="Palette">
+                {/* Navber goes here */}
+
+                <div className="Palette-colors">
+                {/* bunch of color boxes */}
+
+                </div>
+                {/* Footer eventually */}
+                
+            </div>
+        );
+    }
+}
+export default Palette;
