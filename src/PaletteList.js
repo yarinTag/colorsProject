654e3cef221eacf627dc01d8f2cdd72a4845d799
yarinTag <taguori5@gmai.com>
2020-10-17 12:06:21 +0300

    Start styilng ManiPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
+import MiniPalette from './MiniPalette.js';
+
 
 class PaletteList extends Component{
 
@@ -7,11 +9,10 @@ class PaletteList extends Component{
         const {palettes}=this.props;
         return(
             <div>
+                <MiniPalette/>
                 <h1>React Colors</h1>
                 {palettes.map(palette=>(
-                    <p>
-                        <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-                    </p>
+                    <MiniPalette {...palette}/>
                 ))}
                 
             </div>
