259bf51311de9efcab75aeea53d3cbc38786cd47
yarinTag <taguori5@gmai.com>
2020-10-19 22:06:51 +0300

    Add text color on More & Copy & Name Used with chroma-js

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {CopyToClipboard} from "react-copy-to-clipboard";
 import {Link} from 'react-router-dom';
 import "./ColorBox.css";
-
+import chroma from "chroma-js";
 
 class ColorBox extends Component{
     constructor(props){
@@ -17,25 +17,27 @@ class ColorBox extends Component{
     }
 render(){
     const {name, background}=this.props;
+    const isDarkColor=(chroma(background).luminance()) <= 0.067;
+    const isLightColor=(chroma(background).luminance()) >= 0.61;
   return (
       
       <div style={{background: this.props.background}} className="ColorBox">
          <div style={{background}} className={`copy-overlay ${this.state.copied && "show"}`}/>
          <div className={`copy-msg ${this.state.copied && "show"}`}>
              <h1>Copied!</h1>
-             <p>{this.props.background}</p>
+             <p className={isLightColor && "dark-text"}>{this.props.background}</p>
          </div>
          <div className="copy-container">
              <div className="box-content">
-                <span>{name}</span>
+                <span className={isDarkColor && "light-text"}>{name}</span>
              </div>
              <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-             <button className="copy-button">Copy</button>
+             <button className={`copy-button ${isLightColor && "dark-text"}`}>Copy</button>
              </CopyToClipboard>
          </div>
          {this.props.showLink && (
          <Link to={`/palette/${this.props.paletteId}/${this.props.id}`} onClick={e=> e.stopPropagation()}>
-            <span className="see-more">More</span>
+            <span className={`see-more ${isLightColor && "dark-text"}`}>More</span>
          </Link>
          )}
       </div>
